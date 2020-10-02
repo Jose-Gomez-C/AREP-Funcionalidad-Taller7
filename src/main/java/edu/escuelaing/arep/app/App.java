37b37871d0a2187363c00638982cd05b80b3b359
Jose Luis Gomez Camacho <josemania10@gmail.com>
2020-10-01 23:05:04 -0500

    Parte Logica terminad

---
+++ src/main/java/edu/escuelaing/arep/app/App.java
@@ -0,0 +1,27 @@
+package edu.escuelaing.arep.app;
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.secure;
+
+import com.google.gson.Gson;
+
+public class App {
+	public static void main(String[] args) {
+		port(getPort());
+		Gson gson = new Gson();
+		port(getPort());
+		secure("keystores/ecikeystore.p12", "Hola123", null, null);
+		get("/respuesta",(request, response) -> {
+			System.out.println("Llegue");
+			return "<h4>Conectado con otra app</h4>"; 
+		});
+	}
+
+	static int getPort() {
+		if (System.getenv("PORT") != null) {
+			return Integer.parseInt(System.getenv("PORT"));
+		}
+		return 5001;
+	}
+}
